e7f4ccd78217531bb4e1c03c817fd6b60de6ac68
Brie <brie@brie.gay>
2016-10-26 12:49:11 -0400

    added main file

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Wave Function Collapse in Python
+Ideas/Algorithm from github.com/mxgmn/WaveFunctionCollapse
+Brian Van Rosendale
+'''
+
+def main():
+    print('wave function collapse')
+
+
+if __name__ == '__main__':
+    main()
